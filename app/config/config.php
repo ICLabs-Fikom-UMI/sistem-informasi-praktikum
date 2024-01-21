2897f5ec740cbec4abd04cc5a051e985a09d4e5e
nas <me@example.com>
2024-01-21 15:30:24 +0800

    login success

--- app/config/config.php
+++ app/config/config.php
@@ -6,4 +6,4 @@ define('BASEURL','https://localhost/sistem-informasi-praktikum/public');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'siprak');
+define('DB_NAME', 'siprak_db');
--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -2,7 +2,7 @@
 
 class Login extends Controller {
     public function index() {
-        $data['title'] = 'Home';
+        $data['title'] = 'Login';
 
         $this->view('templates/header', $data);
         $this->view('login/index');
@@ -12,5 +12,32 @@ class Login extends Controller {
     public function login() {
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $role = $this->model('Login_model')->getRole($email);
+
+        echo var_dump($_POST);
+        echo "<br></br>";
+
+        $id_user = $this->model('Login_model')->validateLogin($email, $password);
+
+        if ($id_user) {
+            echo "Berhasil Login";
+
+            $is_password_default = $this->model('Login_model')->isDefaultPassword($password);
+            // [PENTINGJ] hapus tanda (!) negasi untuk sementara untuk mengabaikan password default
+            if (!$is_password_default) {
+                // header('Location: change-default-password.php');
+                echo "Password Default";
+                exit();
+            }
+            else {
+                header('Location: ' . BASEURL . '/home');
+                exit;
+            }
+        }
+        else {
+            echo "Email Atau Password Salah";
+        }
     }
+
 }
