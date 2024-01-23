76dcb668a35f17b6aa8f19003ddd44276c0a5180
nas <me@example.com>
2024-01-23 22:01:50 +0800

    update sidebar

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -1,4 +1,4 @@
-<div class="sidenav">
+<!-- <div class="sidenav">
   <div class="menu-item">
     <a href="<?= BASEURL?>/DaftarNilai">Daftar Nilai</a>
   </div>
@@ -14,4 +14,39 @@
   <div class="menu-item">
     <a href="<?= BASEURL?>/Settings">Settings</a>
   </div>
-</div>
+</div> -->
+
+<link rel="stylesheet" href="<?= BASEURL ?>/assets/css/sidebar.css" />
+<body?>
+  <div class="container-fluid">
+    <div class="row">
+      <nav class="col-md-2 d-none d-md-block sidebar">
+        <div class="sidebar-logo">
+          <a href="<?= BASEURL?>/DaftarNilai">
+            <img src="<?= BASEURL ?>/images/logo.png" alt="Logo SIPEMLA" />
+          </a>
+          <h5 class="link-sipemla">
+            SIPRAK
+          </h5>
+        </div>
+        <ul class="sidebar-menu">
+          <li>
+            <a href="<?= BASEURL?>/DaftarNilai"><img src="<?= BASEURL ?>/icons/daftar-nilai.svg" alt="Icon 1" />Beranda</a>
+          </li>
+          <li>
+            <a href="<?= BASEURL?>/AsistenDanDosen"><img src="<?= BASEURL ?>/icons/asisten-dan-dosen.svg" alt="Icon 1" />Asisten dan Dosen</a>
+          </li>
+          <li>
+            <a href="<?= BASEURL?>/DataFrekuensi"><img src="<?= BASEURL ?>/icons/data-frekuensi.svg" alt="Icon 1" />Data Frekuensi</a>
+          </li>
+          <li>
+            <a href="<?= BASEURL?>/Laboratorium"><img src="<?= BASEURL ?>/icons/laboratorium.svg" alt="Icon 1" />Laboratorium</a>
+          </li>
+          <li>
+            <a href="<?= BASEURL?>/Settings"><img src="<?= BASEURL ?>/icons/settings.svg" alt="Icon 1" />Settings</a>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- Content area -->
+      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
