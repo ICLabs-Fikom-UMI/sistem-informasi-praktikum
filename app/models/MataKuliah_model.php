91d72ff210d78aa10ae09b576edbc483bbaedc5a
nas <me@example.com>
2024-02-06 16:52:15 +0800

    error when call DataPerkuliahan_model

--- app/models/MataKuliah_model.php
+++ app/models/MataKuliah_model.php
@@ -18,5 +18,24 @@ class Matakuliah_model {
         return $this->db->resultSet();
     }
 
-    public function 
+    public function getDataGroupByMatkulKelas() {
+        try {
+            $query = 'SELECT dosen.id_dosen, dosen.nama as nama_dosen, mata_kuliah.id_matkul, mata_kuliah.nama_matkul, count(*) as total_mhs FROM kuliah 
+                    JOIN dosen ON dosen.id_dosen = kuliah.id_dosen
+                    JOIN mata_kuliah on mata_kuliah.id_matkul = kuliah.id_matkul
+                    GROUP BY kuliah.id_matkul, kuliah.kelas';
+
+            $this->db->query($query);
+
+            // Check if the query execution was successful
+            if (!$this->db->execute()) {
+                throw new Exception('Failed to execute query: ' . $this->db->getError());
+            }
+
+            return $this->db->resultSet();
+        } catch (Exception $e) {
+            echo 'Error : ' . $e->getMessage();
+        }
+    }
+
 }
